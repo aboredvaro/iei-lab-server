f16bf09e25664efcc84258c63907b33669bb9453
Karmelo1984 <79457759+Karmelo1984@users.noreply.github.com>
2021-11-13 17:51:24 +0100

    Esto es lo ultimo que hago en main
    Me mudo a mi rama,
    
    He dejado cosicas que creo que es importante para todos

---
+++ utils/query.js
@@ -0,0 +1,14 @@
+import log from './log.js'
+
+export async function query(db, sql) {
+	//log(sql)
+	return new Promise(resolve => {
+		db.query(sql, (err, result) => {
+			if (err) {
+				console.log(err)
+				resolve(-1)
+			}
+			resolve(result)
+		})
+	})
+}
